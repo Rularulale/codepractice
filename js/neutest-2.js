e194dda6d1b707ce0fb6057d540c50b66c14b673
Rularulale <piano9327@gmail.com>
2024-01-06 02:10:17 +0800

    addsome

--- js/neutest-2.js
+++ js/neutest-2.js
@@ -1,17 +1,21 @@
-$(document).ready(function () {
-
+document.addEventListener('DOMContentLoaded', function () {
+    var exchangeWiseRadioButton = document.getElementById('consolidated');
+    if (exchangeWiseRadioButton) {
+      exchangeWiseRadioButton.checked = true;
+      exchangeWiseRadioButton.parentElement.classList.add('active');
+      console.log('Consolidated radio button is checked and marked as active.');
+    }
+  
     var radioButtons = document.querySelectorAll('input[name="options"]');
-
     radioButtons.forEach((radioButton) => {
       radioButton.addEventListener('change', function () {
-
         document.querySelectorAll('.radio-button.active').forEach((el) => {
           el.classList.remove('active');
         });
   
         if (this.checked) {
           this.parentElement.classList.add('active');
-          
+          console.log('Active class added to the selected radio button.');
         }
       });
     });
